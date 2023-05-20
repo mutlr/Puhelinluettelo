3220ba777b367007ee4b7661416d2828292fa649
mutlr <rmutlu01@hotmail.com>
2023-05-20 11:27:59 +0300

    Puhelinluettelo backend 3.12

--- mongo.js
+++ mongo.js
@@ -7,26 +7,33 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://uusposti456:${password}@cluster0.lppbvdq.mongodb.net/phoneBook?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: Boolean,
+  number: Number
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  content: 'HTML is Easy',
-  important: true,
+  name: process.argv[3],
+  number: process.argv[4],
 })
-
-person.save().then(result => {
+if (process.argv.length === 3) {
+  Person.find({}).then(result  => {
+      console.log("Phonebook:")
+      result.forEach(person => {
+        console.log(person.name, person.number)
+      })
+      mongoose.connection.close();
+  });
+}
+/*person.save().then(result => {
   console.log(result)
   console.log('note saved!')
   mongoose.connection.close()
-})
+}) */
